782498a45f5d3233b2740fb77f75baa76f35c182
Sergey Syrkov <sircovsw@gmail.com>
2020-10-29 12:04:52 +0300

    url attribute

--- src/widget/Upload.php
+++ src/widget/Upload.php
@@ -158,9 +158,11 @@ class Upload extends InputWidget
                 'editFiledescription' => $this->editFiledescription,
                 'errorHandler' => $this->errorHandler,
                 'pathAttribute' => 'path',
+                'urlAttribute' => 'url',
                 'baseUrlAttribute' => 'base_url',
                 'pathAttributeName' => 'path',
                 'baseUrlAttributeName' => 'base_url',
+                'urlAttributeName' => 'url',
                 'messages' => [
                     'maxNumberOfFiles' => Yii::t($this->messagesCategory, 'Maximum number of files exceeded'),
                     'acceptFileTypes' => Yii::t($this->messagesCategory, 'File type not allowed'),
