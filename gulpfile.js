356192e894277114385a547e3c17cbcce45c0b46
SyrkovSV <sircovsw@gmail.com>
2020-11-16 13:03:34 +0300

    preview file

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,28 @@ var less = require('gulp-less');
 var path = require('path');
 var minifyCSS = require('gulp-minify-css');
 
-gulp.task('js', function() {
-    return gulp.src('./src/widget/assets/js/upload-kit.js')
+gulp.task('js', function (done) {
+    gulp.src('./src/widget/assets/js/upload-kit.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('src/widget/assets/js'));
+    done();
 });
 
-gulp.task('less-min', function () {
-    return gulp.src('./src/widget/assets/css/*.less')
+gulp.task('less-min', function (done) {
+    gulp.src('./src/widget/assets/css/*.less')
         .pipe(less())
         .pipe(minifyCSS())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./src/widget/assets/css'));
+    done();
 });
 
-gulp.task('less', function () {
-    return gulp.src('./src/widget/assets/css/*.less')
+gulp.task('less', function (done) {
+    gulp.src('./src/widget/assets/css/*.less')
         .pipe(less())
         .pipe(gulp.dest('./src/widget/assets/css'));
+    done();
 });
 
-gulp.task('default', ['js', 'less', 'less-min']);
+gulp.task('default', gulp.series('js', 'less', 'less-min'));
